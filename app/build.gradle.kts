2b5bb16bce4897b0eae46294909387e35044e6ca
topdiveair-sketch <topdiveair@gmail.com>
2026-09-18 15:52:43 +0200

    Add secure Android share support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "at.zuhauseambach.windichronik"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildTypes {
@@ -27,5 +27,6 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core:1.15.0")
     implementation("androidx.webkit:webkit:1.12.1")
 }
